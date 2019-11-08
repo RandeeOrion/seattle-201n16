ce2c419f671f0ec2040b9fc81c1aa3c2a13f9513
Randee Orion <randeegood@gmail.com>
2019-11-07 19:28:09 -0800

    quesiton 4 passes

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -91,16 +91,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-var testArray = [2, 3, 4]; //eslint-disable-line
+var testArray = [2,3,4]; //eslint-disable-line
 
-function sumArray(array) { //eslint-disable-line
-    var shotInTheDark = testArray + [i];
-    console.log(shotInTheDark);
+function sumArray(sumAr) { //eslint-disable-line
+    var sum1 = sum(sumAr[0], sumAr[1]);
+    var sum2 = sum(sum1[0], sumAr[2]);
+    //console.log(sum2);
+
+    var stringY = [sum2[0], `${testArray} was passed in as an array of numbers, and ${sum2[0]} is their sum.`];
+    return stringY;
 }
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -151,3 +156,4 @@ function multiplyAnyArray() { //eslint-disable-line
 // testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+//
